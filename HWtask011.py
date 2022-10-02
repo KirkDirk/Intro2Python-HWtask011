6372e92d52acb54d9115457856a05ddd23a01a3f
Kirk Dirk <jackalex@ya.ru>
2022-10-02 09:37:20 +0500

    initial commit

--- HWtask011.py
+++ HWtask011.py
@@ -3,3 +3,16 @@
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+import random
+
+sizelist = int(input('Введите размер списка: '))
+somelist = []
+for i in range(sizelist):
+    somelist.append(i)
+random.shuffle(somelist)
+print(somelist)
+
+sumoddel = 0
+for i in range(1, sizelist, 2):
+    sumoddel += somelist[i]
+print('Сумма нечетных элементов списка: ', sumoddel)
